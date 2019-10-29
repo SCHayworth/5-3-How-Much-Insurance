0caebc2700950df1db97f55e21732d4497d4b05c
Shaun Hayworth <thismoderndeath@gmail.com>
2019-10-28 22:21:10 -0700

    Does not get input for insurance percent

--- 5-3-how-much-insurance.py
+++ 5-3-how-much-insurance.py
@@ -4,7 +4,7 @@
 # 10-28-2019
 # Original source code and revision history can be found at https://github.com/SCHayworth/5-3-How-Much-Insurance
 
-# Calculates the minimum recommended insurance amount based on a percentage of the replacement cost of a building.
+# Calculates the minimum recommended insurance amount based on a 80% of the replacement cost of a building.
 
 # Define the main function
 # Mainline program logic
@@ -14,17 +14,17 @@ def main():
     replacement_amount = int(input('Enter the  replacement amount: '))
 
     # Prompt user for the percentage of insurance
-    ins_percent = int(input('Percent insured: '))
+    print('Percent insured: 80%')
 
     # Call the min_insurance function and pass replacement_amount to it.
-    min_insurance(replacement_amount, ins_percent)
+    min_insurance(replacement_amount)
 
 # Define the min_insurance function
 # Calculates the minimum amount of insurance recommended.
-def min_insurance(replacement_amount, ins_percent):
+def min_insurance(replacement_amount):
 
     # Calculate insurance recommendation
-    ins_rec = replacement_amount * (ins_percent / 100)
+    ins_rec = replacement_amount * 0.8
 
     # Display the recommended amount of insurance
     print(f'Minimum Insurance: ${ins_rec:.2f}')
